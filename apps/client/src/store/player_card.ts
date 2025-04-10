9fb5bf465e923ed4509a81ec9981581c30c8f28f
VictorHazebrouck <hazebrouck.victor@gmail.com>
2025-04-10 16:55:39 +0200

    close player card on move

--- apps/client/src/store/player_card.ts
+++ apps/client/src/store/player_card.ts
@@ -1,6 +1,7 @@
 import type { Postion, SocketData } from "@repo/shared-types/socket";
-import { atom } from "nanostores";
+import { atom, subscribeKeys } from "nanostores";
 import { $players_other_persisted } from "./players_other_persisted";
+import { $player_self } from "./player_self";
 
 export const $player_card = atom<{
     _id: string;
@@ -21,3 +22,8 @@ export function hide_player_card()
 {
     $player_card.set(null);
 }
+
+subscribeKeys($player_self, ["spacial"], () =>
+{
+    hide_player_card();
+});
