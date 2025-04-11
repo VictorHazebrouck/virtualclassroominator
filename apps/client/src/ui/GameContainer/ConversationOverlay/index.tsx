86190221458e0af6831355ebd860d40c410eb30a
VictorHazebrouck <hazebrouck.victor@gmail.com>
2025-04-11 17:24:23 +0200

    add possibility to zoom in on a stream

--- apps/client/src/ui/GameContainer/ConversationOverlay/index.tsx
+++ apps/client/src/ui/GameContainer/ConversationOverlay/index.tsx
@@ -4,6 +4,7 @@ import { Video, VideoLayout } from "~/ui/-components/Video";
 import { tm } from "~/utils/tm";
 import { use_get_user_screenshare_by_id, use_get_user_video_by_id } from "~/stream/hooks";
 import { BiX } from "react-icons/bi";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 export default function ConversationOverlay()
 {
@@ -11,8 +12,6 @@ export default function ConversationOverlay()
         user_id: string;
         type: "screenshare" | "video";
     } | null>(null);
-    const video_track = use_get_user_video_by_id(selected_video?.user_id || "");
-    const screenshare_track = use_get_user_screenshare_by_id(selected_video?.user_id || "");
 
     function handle_select_video(user_id: string, type: "screenshare" | "video")
     {
@@ -33,23 +32,48 @@ export default function ConversationOverlay()
         >
             <ParticipantsList on_select_video={handle_select_video} />
             {selected_video && (
-                <div className="relative flex h-full w-full items-center justify-center p-2 pl-0">
-                    <VideoLayout className="aspect-auto h-full w-full cursor-default">
-                        <button
-                            className="absolute top-2 right-2 z-50 cursor-pointer"
-                            onClick={() => handle_close_video()}
-                        >
-                            <BiX size={32} className="text-white" />
-                        </button>
+                <EnlargedVideo
+                    on_close_video={() => handle_close_video()}
+                    user_id={selected_video.user_id}
+                    type={selected_video.type}
+                />
+            )}
+        </div>
+    );
+}
+
+interface EnlargedVideoProps
+{
+    user_id: string;
+    type: "screenshare" | "video";
+    on_close_video: () => void;
+}
+function EnlargedVideo({ user_id, type, on_close_video }: EnlargedVideoProps)
+{
+    const video_track = use_get_user_video_by_id(user_id);
+    const screenshare_track = use_get_user_screenshare_by_id(user_id);
+
+    return (
+        <div className="relative flex h-full w-full items-center justify-center p-2 pl-0">
+            <VideoLayout className="aspect-auto h-full w-full cursor-move">
+                <button
+                    className="absolute top-2 right-2 z-50 cursor-pointer"
+                    onClick={() => on_close_video()}
+                >
+                    <BiX size={32} className="text-white" />
+                </button>
+                <TransformWrapper initialScale={1} maxScale={2} minScale={1} disablePadding>
+                    <TransformComponent
+                        wrapperStyle={{ width: "100%", height: "100%" }}
+                        contentStyle={{ width: "100%", height: "100%" }}
+                    >
                         <Video
                             className="object-contain"
-                            videotrack={
-                                selected_video.type == "video" ? video_track : screenshare_track
-                            }
+                            videotrack={type == "video" ? video_track : screenshare_track}
                         />
-                    </VideoLayout>
-                </div>
-            )}
+                    </TransformComponent>
+                </TransformWrapper>
+            </VideoLayout>
         </div>
     );
 }
