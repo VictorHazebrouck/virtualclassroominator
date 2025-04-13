6723c44a4ea37799f7bd5a1156dd4bec955c0c58
VictorHazebrouck <hazebrouck.victor@gmail.com>
2025-04-13 11:32:14 +0200

    cap webcam capture quality, add default audio enhancements

--- apps/client/src/stream/utils.ts
+++ apps/client/src/stream/utils.ts
@@ -17,14 +17,27 @@ export async function request_screenshare_track()
 
 export async function request_webcam_track()
 {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: {
+            width: { exact: 240 },
+            frameRate: { exact: 20 },
+        },
+        audio: false,
+    });
     const video_track = stream.getVideoTracks()[0]!;
     return video_track;
 }
 
 export async function request_microphone_track()
 {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const stream = await navigator.mediaDevices.getUserMedia({
+        audio: {
+            echoCancellation: true,
+            noiseSuppression: true,
+            autoGainControl: true,
+        },
+        video: false,
+    });
     const audio_track = stream.getAudioTracks()[0]!;
     return audio_track;
 }
