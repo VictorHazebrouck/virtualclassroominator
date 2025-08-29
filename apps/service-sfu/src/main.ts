9742808ba7e6bc2952d80856c1ad5b3904d95f37
VictorHazebrouck <hazebrouck.victor@gmail.com>
2025-08-29 15:49:49 +0200

    add .env.local for each service to

--- apps/service-sfu/src/main.ts
+++ apps/service-sfu/src/main.ts
@@ -1,8 +1,8 @@
 //livekit stuff
 
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
 
-if (!PORT)
-{
-    throw new Error("No PORT found");
-}
+// if (!PORT)
+// {
+//     throw new Error("No PORT found");
+// }
